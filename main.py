4e20da6e934a9abbc1972c97cc069cf35f8ea7bb
Neznakomec <misha-ershov@ya.ru>
2026-05-11 18:29:46 +0300

    load and save data to files

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from equity_project.src.get_data import get_data
 from equity_project.src.run_backtest import run_backtest
 from equity_project.src.train import train
+from equity_project.src2.get_data_1_raw import load_data
 
 
 def main():
+    load_data()
     get_data()
     train()
     run_backtest()
